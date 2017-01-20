a858a4df05c9fd692386e483a3bb3c9b11721626
Lior Bruder <lior@11sheep.com>
2017-01-20 19:24:19 +0200

    Gravity is working

--- ShipWave3/Assets/Scripts/GameManager.cs
+++ ShipWave3/Assets/Scripts/GameManager.cs
@@ -7,13 +7,21 @@ public class GameManager : MonoBehaviour {
     private static float MIN_SCALE_X = 1f;
     private static float MAX_SCALE_X = 3f;
     private static float MIN_SCALE_Y = 1f;
-    private static float MAX_SCALE_Y = 3f;
+    private static float MAX_SCALE_Y = 1f;
 
     private float mShipSpeed = 2f;
+    private float mShipTurnSpeed = 100;
+
+    private float mCurrentSlipAngle = 0;
+    private float mCurrentShipAngle = 0;
+
+    private int mCurrentPartIndex = 0;
 
     private static int NUM_OF_OBJECTS = 20;
 
     private GameObject mShip;
+    private GameObject mAll;
+    private GameObject mCamera;
 
     float currentX = 0;
     float currentY = 0;
@@ -21,12 +29,18 @@ public class GameManager : MonoBehaviour {
     float lastScaleY = 0;
     bool lastOneWasUP = false;
 
+    bool mDirectionIsUp;
+
     private PathPartInfo[] mParts;
 
     // Use this for initialization
     void Start () {
 
+        Debug.Log("gravity: " + Physics2D.gravity);
+
         mShip = GameObject.Find("TheShip");
+        mAll = GameObject.Find("All");
+        mCamera = GameObject.Find("Main Camera");
 
         mParts = new PathPartInfo[NUM_OF_OBJECTS];
 
@@ -35,6 +49,8 @@ public class GameManager : MonoBehaviour {
             GameObject container = new GameObject();
             GameObject triangle = Instantiate(Resources.Load("triangle1", typeof(GameObject))) as GameObject;
             GameObject box = Instantiate(Resources.Load("black_square", typeof(GameObject))) as GameObject;
+
+            container.transform.parent = mAll.transform;
 
             triangle.transform.parent = container.transform;
             box.transform.parent = container.transform;
@@ -60,6 +76,31 @@ public class GameManager : MonoBehaviour {
 
         mShip.transform.Translate(Vector2.right * Time.deltaTime * mShipSpeed);
         mShip.transform.localPosition = new Vector2(mShip.transform.localPosition.x, GetShipHeightAccordingToXPosition());
+
+        // Take the ship to the right angle
+        if (mCurrentShipAngle > mCurrentSlipAngle)
+        {
+            mCurrentShipAngle -= Time.deltaTime * mShipTurnSpeed;
+        }
+        else if (mCurrentShipAngle <= mCurrentSlipAngle)
+        {
+            mCurrentShipAngle += Time.deltaTime * mShipTurnSpeed;
+        }
+
+        if (mDirectionIsUp)
+        {
+            mShip.transform.localEulerAngles = new Vector3(mShip.transform.localEulerAngles.x, mShip.transform.localEulerAngles.y, mCurrentShipAngle - 90);       
+        }
+        else
+        {
+            mShip.transform.localEulerAngles = new Vector3(mShip.transform.localEulerAngles.x, mShip.transform.localEulerAngles.y, mCurrentShipAngle - 90);
+        }
+
+        if (Input.gyro.enabled)
+        {
+            RotateScreen(Input.gyro.rotationRate.normalized.x);
+            Debug.Log("x: " + Input.gyro.rotationRate.normalized.x + ", y: " + Input.gyro.rotationRate.normalized.y);
+        }
     }
 
     private void AddToPath()
@@ -165,13 +206,15 @@ public class GameManager : MonoBehaviour {
                    ((mParts[index].triangle.transform.localPosition.x - Mathf.Abs(mParts[index].scaleX / 2)) <= shipCurrentX)) 
 
             {
-                bool isGoingUP = (mParts[index].scaleX < 0);
+                mCurrentPartIndex = index;
+
+                mDirectionIsUp = (mParts[index].scaleX < 0);
 
                 // Found the position, now find the exact hight
                 float positionInChunk;
                 float relativeXPosition;
 
-                if (isGoingUP)
+                if (mDirectionIsUp)
                 {
                     positionInChunk = Mathf.Abs(mParts[index].scaleX) - (mParts[index].triangle.transform.localPosition.x + Mathf.Abs(mParts[index].scaleX / 2) - shipCurrentX);
                     relativeXPosition = (positionInChunk / Mathf.Abs(mParts[index].scaleX));
@@ -184,14 +227,66 @@ public class GameManager : MonoBehaviour {
 
                 returnHeight = mParts[index].triangle.transform.localPosition.y + (relativeXPosition * mParts[index].scaleY) - mParts[index].scaleY/2;
 
-                Debug.Log("...");
-                Debug.Log("x min: " + (mParts[index].triangle.transform.localPosition.x - Mathf.Abs(mParts[index].scaleX / 2)));
-                Debug.Log("x max: " + (mParts[index].triangle.transform.localPosition.x + Mathf.Abs(mParts[index].scaleX / 2)));
-                Debug.Log("Index found: " + index + ",shipCurrentX: " + shipCurrentX + ", height: " + returnHeight);
+                // Debug.Log("...");
+                //Debug.Log("x min: " + (mParts[index].triangle.transform.localPosition.x - Mathf.Abs(mParts[index].scaleX / 2)));
+                //Debug.Log("x max: " + (mParts[index].triangle.transform.localPosition.x + Mathf.Abs(mParts[index].scaleX / 2)));
+                //Debug.Log("Index found: " + index + ",shipCurrentX: " + shipCurrentX + ", height: " + returnHeight);
+
+                if (mDirectionIsUp)
+                {
+                    mCurrentSlipAngle = 90 - Mathf.Atan(mParts[index].scaleY / mParts[index].scaleX) * 180 / Mathf.PI;
+                }
+                else
+                {
+                    mCurrentSlipAngle = 90 - Mathf.Atan(mParts[index].scaleY / mParts[index].scaleX) * 180 / Mathf.PI;
+                }
+                //mCurrentSlipAngle = (slipAngle < 0) ? (slipAngle + 360) : slipAngle;
+                //Debug.Log("angle: " + mCurrentSlipAngle + ", ship angle: " + mCurrentShipAngle);
                 break;
             }
         }
 
         return returnHeight;
+    }
+    /*
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(200, 70, 100, 100), "Rotate right"))
+        {
+            mCamera.transform.Rotate(Vector3.forward, 10);
+            mShip.transform.Rotate(Vector3.forward, 10);
+
+            Vector2 dir = (Vector2)(Quaternion.Euler(0, 0, mShip.transform.localEulerAngles.z + 90) * Vector2.right);
+            Physics2D.gravity = -dir * 9.8f;
+
+            Debug.Log("gravity: " + Physics2D.gravity);
+            //Physics2D.gravity = new Vector2(mParts[mCurrentPartIndex].scaleX, mParts[mCurrentPartIndex].scaleY);
+        }
+
+        if (GUI.Button(new Rect(50, 70, 100, 100), "Rotate left"))
+        {
+            mCamera.transform.Rotate(Vector3.forward, -10);
+            mShip.transform.Rotate(Vector3.forward, -10);
+
+            Vector2 dir = (Vector2)(Quaternion.Euler(0, 0, mShip.transform.localEulerAngles.z + 90) * Vector2.right);
+            Physics2D.gravity = -dir * 9.8f;
+
+            Debug.Log("gravity: " + Physics2D.gravity);
+            //Physics2D.gravity = new Vector2(mParts[mCurrentPartIndex].scaleX, mParts[mCurrentPartIndex].scaleY);
+        }
+
+
+    }
+    */
+
+    private void RotateScreen(float angle)
+    {
+        mCamera.transform.Rotate(Vector3.forward, angle);
+        mShip.transform.Rotate(Vector3.forward, angle);
+
+        Vector2 dir = (Vector2)(Quaternion.Euler(0, 0, mShip.transform.localEulerAngles.z + 90) * Vector2.right);
+        Physics2D.gravity = -dir * 9.8f;
+
+        //Debug.Log("gravity: " + Physics2D.gravity);
     }
 }
