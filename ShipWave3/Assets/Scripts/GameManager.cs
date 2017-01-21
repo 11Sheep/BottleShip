c9bcd5fba4f38ff2ec8957c87b8c44839dc04dd4
Lior Bruder <lior@11sheep.com>
2017-01-21 06:35:25 +0200

    ready for graphics

--- ShipWave3/Assets/Scripts/GameManager.cs
+++ ShipWave3/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -10,14 +11,15 @@ public class GameManager : MonoBehaviour {
     private static float MAX_SCALE_Y = 1f;
 
     private float mShipSpeed = 2f;
-    private float mShipTurnSpeed = 100;
+    private float mShipTurnSpeed = 20;
 
     private float mCurrentSlipAngle = 0;
     private float mCurrentShipAngle = 0;
+    private float mShipAngleExtra = 0; // The adding of the user pressing the buttons
 
     private int mCurrentPartIndex = 0;
 
-    private static int NUM_OF_OBJECTS = 20;
+    private static int NUM_OF_OBJECTS = 40;
 
     private GameObject mShip;
     private GameObject mAll;
@@ -33,12 +35,11 @@ public class GameManager : MonoBehaviour {
 
     private PathPartInfo[] mParts;
 
+    private bool mLeftButtonPressed = false;
+    private bool mRightButtonPressed = false;
+
     // Use this for initialization
     void Start () {
-
-        Debug.Log("gravity: " + Physics2D.gravity);
-
-        Input.gyro.enabled = true;
 
         mShip = GameObject.Find("TheShip");
         mAll = GameObject.Find("All");
@@ -58,13 +59,20 @@ public class GameManager : MonoBehaviour {
             box.transform.parent = container.transform;
 
             mParts[index] = new PathPartInfo();
-            mParts[index].CreateRandomPart();
+            mParts[index].CreateFlatPart();
             mParts[index].triangle = triangle;
             mParts[index].box = box;
             mParts[index].container = container;
         }
 
         CreatePath();
+
+        // Put the ship in the middle
+        mShip.transform.position = new Vector2(mParts[NUM_OF_OBJECTS / 2].triangle.transform.localPosition.x, 0);
+
+        // Put the cargo on the ship
+        GameObject AnimalBox = Instantiate(Resources.Load("Animal1", typeof(GameObject))) as GameObject;
+        //AnimalBox.transform.position = 
     }
 
     void Update()
@@ -75,6 +83,15 @@ public class GameManager : MonoBehaviour {
             // AddToPath();
             mShip.transform.localPosition = new Vector2(mShip.transform.localPosition.x, GetShipHeightAccordingToXPosition());
         }*/
+
+        if (mLeftButtonPressed)
+        {
+            mShipAngleExtra -= 1;
+        }
+        else if (mRightButtonPressed)
+        {
+            mShipAngleExtra += 1;
+        }
 
         mShip.transform.Translate(Vector2.right * Time.deltaTime * mShipSpeed);
         mShip.transform.localPosition = new Vector2(mShip.transform.localPosition.x, GetShipHeightAccordingToXPosition());
@@ -98,11 +115,13 @@ public class GameManager : MonoBehaviour {
             mShip.transform.localEulerAngles = new Vector3(mShip.transform.localEulerAngles.x, mShip.transform.localEulerAngles.y, mCurrentShipAngle - 90);
         }
 
+        /*
         if (Input.gyro.enabled)
         {
             RotateScreen(Input.gyro.rotationRate.normalized.z);
             Debug.Log("x: " + Input.gyro.rotationRate.normalized.x + ", y: " + Input.gyro.rotationRate.normalized.y);
         }
+        */
     }
 
     private void AddToPath()
@@ -117,7 +136,7 @@ public class GameManager : MonoBehaviour {
 
         mParts[NUM_OF_OBJECTS - 1] = firstOne;
 
-        AddOnePath(NUM_OF_OBJECTS - 1);
+        AddOnePath(NUM_OF_OBJECTS - 1, true);
     }
 
     private void CreatePath()
@@ -128,8 +147,13 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private void AddOnePath(int index)
+    private void AddOnePath(int index, bool makeRandomParams = false)
     {
+        if (makeRandomParams)
+        {
+            mParts[index].CreateRandomPart();
+        }
+
         currentX += (Mathf.Abs(lastScaleX / 2)) + (Mathf.Abs(mParts[index].scaleX) / 2);
 
         bool goUp = (mParts[index].scaleX < 0);
@@ -161,18 +185,13 @@ public class GameManager : MonoBehaviour {
         mParts[index].triangle.transform.localScale = new Vector2(mParts[index].scaleX, mParts[index].scaleY);
 
         // Set the box
-        float scaleOfBox = 15;
+        float scaleOfBox = 30;
         mParts[index].box.transform.localScale = new Vector2(mParts[index].triangle.transform.localScale.x, scaleOfBox);
         mParts[index].box.transform.localPosition = new Vector2(mParts[index].triangle.transform.localPosition.x, mParts[index].triangle.transform.localPosition.y - mParts[index].triangle.transform.localScale.y / 2 - scaleOfBox / 2);
 
         lastScaleX = mParts[index].scaleX;
         lastScaleY = mParts[index].scaleY;
         lastOneWasUP = goUp;
-    }
-
-    private void MoveObjectToFuturePosition()
-    {
-
     }
 
     private class PathPartInfo
@@ -192,6 +211,14 @@ public class GameManager : MonoBehaviour {
             direction = (Random.Range((int)0, (int)2) == 0) ? DirectionEnum.GoingUp : DirectionEnum.GoingDown;
             scaleX = Random.Range(MIN_SCALE_X, MAX_SCALE_X);
             scaleY = Random.Range(MIN_SCALE_Y, MAX_SCALE_Y);
+            scaleX = (Random.Range(0, 2) == 0) ? scaleX : -scaleX;
+        }
+
+        public void CreateFlatPart()
+        {
+            direction = DirectionEnum.GoingUp;
+            scaleX = Random.Range(MIN_SCALE_X, MAX_SCALE_X);
+            scaleY = 0.1f;
             scaleX = (Random.Range(0, 2) == 0) ? scaleX : -scaleX;
         }
     }
@@ -242,8 +269,18 @@ public class GameManager : MonoBehaviour {
                 {
                     mCurrentSlipAngle = 90 - Mathf.Atan(mParts[index].scaleY / mParts[index].scaleX) * 180 / Mathf.PI;
                 }
+
+                // Add the user extra power
+                mCurrentShipAngle += mShipAngleExtra;
+                mShipAngleExtra = 0;
                 //mCurrentSlipAngle = (slipAngle < 0) ? (slipAngle + 360) : slipAngle;
-                //Debug.Log("angle: " + mCurrentSlipAngle + ", ship angle: " + mCurrentShipAngle);
+                Debug.Log("angle: " + mCurrentSlipAngle + ", ship angle: " + mCurrentShipAngle);
+
+                // Checj if need to add another block at the end (if we are in the middle)
+                if (index == (NUM_OF_OBJECTS / 2))
+                {
+                    AddToPath();
+                }
                 break;
             }
         }
@@ -291,12 +328,36 @@ public class GameManager : MonoBehaviour {
 
     private void RotateScreen(float angle)
     {
-        mCamera.transform.Rotate(Vector3.forward, angle);
-        mShip.transform.Rotate(Vector3.forward, angle);
+        mCamera.transform.localEulerAngles = new Vector3(0, 0, mCamera.transform.localEulerAngles.z + angle);
+        //mCamera.transform.Rotate(Vector3.forward, angle);
+        //mShip.transform.Rotate(Vector3.forward, angle);
+        mShip.transform.localEulerAngles = new Vector3(0, 0, mShip.transform.localEulerAngles.z + angle);
+        //mShip.transform.localEulerAngles = new Vector3(0, 0, angle);
 
         Vector2 dir = (Vector2)(Quaternion.Euler(0, 0, mShip.transform.localEulerAngles.z + 90) * Vector2.right);
         Physics2D.gravity = -dir * 9.8f;
 
         //Debug.Log("gravity: " + Physics2D.gravity);
     }
+
+    public void OnUIShipLeftPressed()
+    {
+        mLeftButtonPressed = true;
+    }
+
+    public void OnUIShipLeftReleased()
+    {
+        mLeftButtonPressed = false;
+    }
+
+    public void OnUIShipRightPressed()
+    {
+        mRightButtonPressed = true;
+    }
+
+    public void OnUIShipRightReleased()
+    {
+        mRightButtonPressed = false;
+    }
+
 }
